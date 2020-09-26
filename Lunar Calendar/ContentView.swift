2a80a80fc1051388995ba6ebe26203fa5f730f44
Huy Ta <huytbt@gmail.com>
2020-09-26 15:41:37 +0700

    Initial

---
+++ Lunar Calendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lunar Calendar
+//
+//  Created by Huy Ta on 9/26/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lunar Calendar/Lunar_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lunar_CalendarApp.swift
+//  Lunar Calendar
+//
+//  Created by Huy Ta on 9/26/20.
+//
+
+import SwiftUI
+
+@main
+struct Lunar_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
