3154a27a83c1b724c5d00f786e8ab5a1ad4d2c4e
Huy Ta <huytbt@gmail.com>
2020-09-30 11:57:35 +0700

    Week View logic

--- LunarCalendar/Calendar/WeekView.swift
+++ LunarCalendar/Calendar/WeekView.swift
@@ -10,15 +10,25 @@ import SwiftDate
 
 struct WeekView: View {
     let date: Date
+    var selectedDate: Date? = nil
     
-    init(date: Date) {
+    init(date: Date, selectedDate: Date? = nil) {
         self.date = date.dateAt(.startOfWeek)
+        self.selectedDate = selectedDate
     }
 
+    fileprivate func dateViewByIndex(_ index: Int) -> DateView {
+        let date = self.date.dateByAdding(index, .day).date
+        return DateView(
+            date: date,
+            selected: selectedDate?.compare(toDate: date, granularity: .day) == .orderedSame
+        )
+    }
+    
     var body: some View {
         HStack {
             ForEach(0 ..< 7) { index in
-                DateView(date: date.dateByAdding(index, .day).date)
+                dateViewByIndex(index)
             }
         }
     }
@@ -26,6 +36,17 @@ struct WeekView: View {
 
 struct WeekView_Previews: PreviewProvider {
     static var previews: some View {
-        WeekView(date: Date())
+        VStack{
+            WeekView(date: Date()).padding()
+            WeekView(date: Date(), selectedDate: Date()).padding()
+            WeekView(date: Date(), selectedDate: Date().dateByAdding(1, .day).date).padding()
+            WeekView(date: Date(), selectedDate: Date().dateByAdding(-1, .day).date).padding()
+        }
+        VStack{
+            WeekView(date: Date()).padding()
+            WeekView(date: Date(), selectedDate: Date()).padding()
+            WeekView(date: Date(), selectedDate: Date().dateByAdding(1, .day).date).padding()
+            WeekView(date: Date(), selectedDate: Date().dateByAdding(-1, .day).date).padding()
+        }.preferredColorScheme(.dark)
     }
 }
